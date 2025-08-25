7bcbdbd18a3fe76c108368f1ecd7086784ce0a1a
minseo <yeominseo59618@gmail.com>
2025-08-25 23:11:56 +0900

    feat: viewsplit 구현

--- src/app/(pages)/(mainpage)/page.tsx
+++ src/app/(pages)/(mainpage)/page.tsx
@@ -1,12 +1,12 @@
 // src/app/page.tsx
 import TopBar from "@/app/_common/components/top-bar";
 import UnderBar from "@/app/_common/components/under-bar";
-import { SplitViewer } from "./components/SplitViewer";
+
 export default function HomePage() {
   return (
     <div>
       <TopBar />
-      <SplitViewer />
+
       <UnderBar />
     </div>
   );
